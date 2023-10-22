7034d18a67b8f949993cca40991d5db79a914e82
MelnykIgor <melnikigor908@gmail.com>
2023-10-22 14:20:22 -0400

    fixed

--- sripts/rock-paper-scssors.js
+++ sripts/rock-paper-scssors.js
@@ -51,8 +51,8 @@ function playGame(playerMove) {
   document.querySelector(".js-result").innerHTML = result;
 
   document.querySelector(".js-moves").innerHTML = `You 
-<img src="./images/${playerMove}-emoji.png" class="move-icon">
-<img src="./images/${computerMove}-emoji.png" class="move-icon">
+<img src="./images/${playerMove.toLowerCase()}-emoji.png" class="move-icon">
+<img src="./images/${computerMove.toLowerCase()}-emoji.png" class="move-icon">
 Computer`;
 }
 function updateScoreElement() {
